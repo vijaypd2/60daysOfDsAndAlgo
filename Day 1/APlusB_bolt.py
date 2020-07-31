bd6ba1a6447943c1ff0294e12cf272d718134841
Restyled.io <commits@restyled.io>
2020-07-31 07:48:37 +0000

    Restyled by black

--- Day 1/APlusB_bolt.py
+++ Day 1/APlusB_bolt.py
@@ -3,4 +3,4 @@
 print("enter the nos :")
 a = int(input("first no:"))
 b = int(input("second no:"))
-print(" the sum of two nos are: {}".format(a+b))
+print(" the sum of two nos are: {}".format(a + b))
